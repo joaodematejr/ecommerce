79cf488318a7b84d5acb58dc7f0f8c881b66925b
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-07-18 14:37:30 -0300

    create modules and config datebase

--- ecommerce/src/app.module.ts
+++ ecommerce/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'shoppingDB',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
